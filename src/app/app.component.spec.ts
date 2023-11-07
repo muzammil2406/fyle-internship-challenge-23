87dc700faebc9f43bbe69dd7f9e784e03fd60b87
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2023-11-08 00:03:42 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,69 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ApiService } from './services/api.service';
+import { of, throwError } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let apiService: ApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [ApiService],
+      imports: [HttpClientModule],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    apiService = TestBed.inject(ApiService);
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'fyle-frontend-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fyle-frontend-challenge');
+  it('should initialize properties', () => {
+    expect(component.username).toEqual('johnpapa');
+    expect(component.userData).toBeUndefined();
+    expect(component.repos).toEqual([]);
+    expect(component.repoLanguages).toEqual({});
+    expect(component.pageSize).toEqual(10);
+    expect(component.currentPage).toEqual(1);
+    expect(component.pagedRepos).toEqual([]);
+    expect(component.userNotFound).toBeFalse();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fyle-frontend-challenge app is running!');
+  it('should call getUserData on ngOnInit', () => {
+    spyOn(component, 'getUserData');
+    component.ngOnInit();
+    expect(component.getUserData).toHaveBeenCalled();
   });
+
+  it('should fetch user data and repositories successfully', () => {
+    const userData = { login: 'johnpapa' };
+    const repos = [{ name: 'repo1' }, { name: 'repo2' }];
+    spyOn(apiService, 'getUser').and.returnValue(of(userData));
+    spyOn(apiService, 'getRepos').and.returnValue(of(repos));
+
+    component.getUserData();
+
+    expect(component.userData).toEqual(userData);
+    expect(component.repos).toEqual(repos);
+    expect(component.userNotFound).toBeFalse();
+  });
+
+  it('should handle an error when fetching user data', () => {
+    spyOn(apiService, 'getUser').and.returnValue(throwError('Error'));
+    spyOn(apiService, 'getRepos').and.returnValue(of([]));
+
+    component.getUserData();
+
+    expect(component.userData).toBeNull();
+    expect(component.repos).toEqual([]);
+    expect(component.userNotFound).toBeTrue();
+  });
+  
 });
