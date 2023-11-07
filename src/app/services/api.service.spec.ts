0c7b30c81a551b1d950f58f7b85c50fe8fe4d95e
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2023-11-08 00:06:24 +0530

    Update api.service.spec.ts

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,96 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { mockReposData, mockRepoLanguagesData } from './mock-data';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch user data', () => {
+    const username = 'johnpapa';
+
+    service.getUser(username).subscribe(user => {
+      expect(user.login).toEqual(username);
+    });
+
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush({ login: username });
+  });
+  it('should handle errors when fetching user data', () => {
+    const username = 'johnpapa';
+  
+    service.getUser(username).subscribe(
+      () => {
+        fail('Expected an error, but received data');
+      },
+      error => {
+        expect(error).toBeTruthy();
+      }
+    );
+  
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}`);
+    expect(req.request.method).toEqual('GET');
+    req.error(new ErrorEvent('network error'), { status: 404, statusText: 'Not Found' });
+  });
+  
+
+  it('should fetch user repositories', () => {
+    const username = 'johnpapa';
+
+    service.getRepos(username).subscribe(repos => {
+      expect(repos).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}/repos`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockReposData);
+  });
+
+  it('should handle errors when fetching user repositories', () => {
+    const username = 'johnpapa';
+
+    service.getRepos(username).subscribe(
+      () => {
+        fail('Expected an error, but received data');
+      },
+      error => {
+        expect(error).toBeTruthy();
+      }
+    );
+
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}/repos`);
+    expect(req.request.method).toEqual('GET');
+    req.error(new ErrorEvent('network error'));
+  });
+
+  it('should fetch repository languages', () => {
+    const username = 'johnpapa';
+    const repoName = 'angular';
+
+    service.getRepoLanguages(username, repoName).subscribe(languages => {
+      expect(languages).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne(`https://api.github.com/repos/${username}/${repoName}/languages`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockRepoLanguagesData);
   });
 });
