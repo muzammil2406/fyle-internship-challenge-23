23b0b6ff5a99ebf06fba979f4f14b85694b89dce
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2023-11-08 00:07:14 +0530

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-
-  getUser(githubUsername: string) {
+  getUser(githubUsername: string): Observable<any> {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string): Observable<any[]> {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`)
+      .pipe(
+        map((data: any) => {
+          return data;
+        }),
+        catchError((error) => {
+          console.error('Error fetching repositories', error);
+          return throwError('Unable to fetch repositories. Please try again.');
+        })
+      );
+  }
+  getRepoLanguages(githubUsername: string, repoName: string): Observable<any> {
+    return this.httpClient.get(`https://api.github.com/repos/${githubUsername}/${repoName}/languages`);
+  }
 }
