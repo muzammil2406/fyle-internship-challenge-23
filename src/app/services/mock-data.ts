c602334b8ea739bedd718687bf41307c1b6df8ca
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2023-11-08 00:08:05 +0530

    Create mock-data.ts

---
+++ src/app/services/mock-data.ts
@@ -0,0 +1,20 @@
+export const mockUserData = {
+  login: 'johnpapa',
+  name: 'John Papa',
+};
+
+export const mockReposData = [
+  {
+    name: 'repo1',
+  },
+  {
+    name: 'repo2',
+  },
+];
+
+export const mockRepoLanguagesData = {
+  TypeScript: 50000,
+  JavaScript: 25000,
+  Python: 20000,
+  SCSS: 5000
+};
